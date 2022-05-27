8d82ef479e24deb14b26c9a4a646fa3ae2ab0803
Deadlye <kevin.rothenbuhler@gmail.com>
2022-05-27 11:46:51 +0200

    Set up Dagger-Hilt

---
+++ app/src/main/java/ch/kra/clothes/ClotesApp.kt
@@ -0,0 +1,8 @@
+package ch.kra.clothes
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ClotesApp: Application() {
+}
---
+++ app/src/main/java/ch/kra/clothes/cloth_list/di/ClotheListModule.kt
@@ -0,0 +1,10 @@
+package ch.kra.clothes.cloth_list.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ClotheListModule {
+}
